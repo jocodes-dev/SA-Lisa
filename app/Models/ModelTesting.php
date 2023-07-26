fd06a49926dbc57c640ed563cefbb334fc00fb6b
akbar tri wicaksono <akbartriwicaksono46@gmail.com>
2023-07-26 22:15:28 +0800

    update

---
+++ app/Models/ModelTesting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelTesting extends Model
+{
+    use HasFactory;
+}
