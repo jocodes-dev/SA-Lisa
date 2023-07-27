e1f19816742d853ce7bb8906f961b1bbbc93012f
akbar tri wicaksono <akbartriwicaksono46@gmail.com>
2023-07-27 12:33:45 +0800

    crud jenis surat

---
+++ app/Models/JenisSuratModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisSuratModel extends Model
+{
+    use HasFactory;
+    protected $table = 'tb_jenis_surat';
+    protected $fillable = [
+        'id' , 'uuid' , 'jenis_surat'
+    ];
+}
