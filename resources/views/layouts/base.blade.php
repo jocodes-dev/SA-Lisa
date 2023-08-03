f36243efce5bb3786f034eaa92b25a39b9360d79
fandi-ahmad <fandi4160@gmail.com>
2023-08-03 18:52:35 +0800

    table in arsip + change loading icon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -38,13 +38,52 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
+<style>
+.loader {
+  display: block;
+  --height-of-loader: 4px;
+  --loader-color: #0071e2;
+  width: 130px;
+  height: var(--height-of-loader);
+  border-radius: 30px;
+  background-color: rgba(0,0,0,0.2);
+  position: relative;
+}
+
+.loader::before {
+  content: "";
+  position: absolute;
+  background: var(--loader-color);
+  top: 0;
+  left: 0;
+  width: 0%;
+  height: 100%;
+  border-radius: 30px;
+  animation: moving 1s ease-in-out infinite;
+  ;
+}
+
+@keyframes moving {
+  50% {
+    width: 100%;
+  }
+
+  100% {
+    width: 0;
+    right: 0;
+    left: unset;
+  }
+}
+</style>
+
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-                height="60" width="60">
+            <div class="loader"></div>
+            {{-- <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
+                height="60" width="60"> --}}
         </div>
 
         <!-- Navbar -->
