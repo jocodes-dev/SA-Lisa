7cdcfc38512843fa16abe4fde2971f672ddbe8dd
Suryawandivta <gitdivta@gmail.com>
2023-08-04 01:48:19 +0800

    menambahkan text selamat datang user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -48,7 +48,23 @@
     </div>
   </div>
 </div>
+<div class="col-lg-12 mb-4 order-0">
+<div class="card">
+<div class="d-flex align-items-end row">
+    <div class="col-sm-7">
+        <div class="card-body">
+            <h5 class="card-title text-primary">Selamat Datang Di Dashboard 🎉</h5>
 
+            @auth
+                      <p class="mb-4">{{ auth()->user()->name }}</p>
+            @endauth
+
+            <i class="fa-sharp fa-solid fa-face-smile text-warning"></i>
+            <a href="javascript:;" class="">Enjoy your work !!!</a>
+        </div>
+    </div>
+</div>
+</div>
 <script>
 $(document).ready(function() {
     $.ajax({
